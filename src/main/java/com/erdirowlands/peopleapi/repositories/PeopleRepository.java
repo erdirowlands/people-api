bde95e7e694be96fb4cabac30c3f90bf93776091
Erdi Rowlands <erdi.rowlands@liberty-it.co.uk>
2021-08-29 20:45:05 +0100

    GGP-13932 Remove unused import

--- src/main/java/com/erdirowlands/peopleapi/repositories/PeopleRepository.java
+++ src/main/java/com/erdirowlands/peopleapi/repositories/PeopleRepository.java
@@ -2,7 +2,6 @@ package com.erdirowlands.peopleapi.repositories;
 
 import com.erdirowlands.peopleapi.entities.person.Person;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 public interface PeopleRepository extends MongoRepository<Person, String> {
