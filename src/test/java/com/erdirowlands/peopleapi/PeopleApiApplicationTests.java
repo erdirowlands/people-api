50624c81ee8b0e202783a28f7cc69ad16fd225f6
Erdi Rowlands <erdi.rowlands@liberty-it.co.uk>
2021-08-29 16:16:24 +0100

    GGP-13932 Add project from initializ

---
+++ src/test/java/com/erdirowlands/peopleapi/PeopleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdirowlands.peopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
