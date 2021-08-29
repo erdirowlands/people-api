993aa15ccabf06b370cbffa6bd507e52c6ffa525
Erdi Rowlands <erdi.rowlands@liberty-it.co.uk>
2021-08-29 16:35:46 +0100

    GGP-13932 Change context path to /app

---
+++ src/main/java/com/erdirowlands/peopleapi/controllers/PeopleController.java
@@ -0,0 +1,9 @@
+package com.erdirowlands.peopleapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+}
