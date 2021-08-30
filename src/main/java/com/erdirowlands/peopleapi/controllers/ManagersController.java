9263b81f36c90761ad0be44f09bd624ef3dbd627
Erdi Rowlands <erdi.rowlands@liberty-it.co.uk>
2021-08-30 01:26:34 +0100

    GGP-13932 Add pagination for manager with no employees

---
+++ src/main/java/com/erdirowlands/peopleapi/controllers/ManagersController.java
@@ -0,0 +1,64 @@
+package com.erdirowlands.peopleapi.controllers;
+
+import com.erdirowlands.peopleapi.entities.manager.Manager;
+import com.erdirowlands.peopleapi.entities.person.Person;
+import com.erdirowlands.peopleapi.repositories.ManagerRepository;
+import com.erdirowlands.peopleapi.services.PeopleService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+public class ManagersController {
+
+    private final ManagerRepository managerRepository;
+
+    public ManagersController(ManagerRepository managerRepository) {
+        this.managerRepository = managerRepository;
+    }
+
+    @GetMapping("app/managers")
+    public ResponseEntity<Map<String, Object>> findManagersWithNoEmployees(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "4") int size) throws Exception {
+
+        try {
+            List<Manager> managers;
+            Pageable paging = PageRequest.of(page, size);
+            Page<Manager> pageManagers;
+            pageManagers = this.managerRepository.findManagerByEmployeesIsNull(paging);
+            managers = pageManagers.getContent();
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("managers", managers);
+            response.put("currentPage", pageManagers.getNumber());
+            response.put("totalItems", pageManagers.getTotalElements());
+            response.put("totalPages", pageManagers.getTotalPages());
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @PostMapping("app/managers")
+    public ResponseEntity<Manager> createPerson(@RequestBody Manager manager) throws Exception {
+        try {
+            Manager newManager = managerRepository.save(new Manager(manager.getName(), manager.getAge(), manager.getEmployees(),
+                    manager.getEmail(), manager.getAddress()));
+            return new ResponseEntity<>(newManager, HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+}
--- src/main/java/com/erdirowlands/peopleapi/entities/manager/Manager.java
+++ src/main/java/com/erdirowlands/peopleapi/entities/manager/Manager.java
@@ -1,30 +1,31 @@
-package com.erdirowlands.peopleapi.entities.person;
+package com.erdirowlands.peopleapi.entities.manager;
 
+import com.erdirowlands.peopleapi.entities.person.Person;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
-@Document(collection = "awesomeCommunity")
-public class Person {
+@Document(collection = "awesomeCollection")
+public class Manager {
 
     @Id
     private String _id;
     private String name;
     private String age;
-    private String balance;
+    private List<Person> employees;
     private String email;
     private String address;
 
-    public Person(String name, String age, String balance, String email, String address) {
+    public Manager(String name, String age, List<Person> employees, String email, String address) {
         this.name = name;
         this.age = age;
-        this.balance = balance;
+        this.employees = employees;
         this.email = email;
         this.address = address;
     }
 
-    public Person() {
 
-    }
 }
